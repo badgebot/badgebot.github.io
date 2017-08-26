ccd69c1e2330cba121ff4d27285fd5ba98fe2e4d
yeezus <yeezus@github.com>
2017-08-26 17:26:00 +0000

    add UTM tags

--- load.js
+++ load.js
@@ -1,9 +1,10 @@
 $( document ).ready(function() {
     var body = $("body"),
     author = $('#author')
+        utmParams = "?utm_source=portfolio&utm_medium=referral&utm_campaign=api-credit"
         unsplashImg = "https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&s=2d58953ffeb732135002a465be8230d3"
         unsplashAuthor = "Dustin Lee"
-        unsplashLink = "https://unsplash.com/@dustinlee"
+        unsplashLink = "https://unsplash.com/@dustinlee" + utmParams
         author.text(unsplashAuthor)
         author.attr("href", unsplashLink)
         body.css('background-image', 'url(' + unsplashImg + ')');
